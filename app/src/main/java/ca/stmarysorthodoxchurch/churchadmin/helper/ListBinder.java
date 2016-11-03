c4896a1910c659099072e2643666983175e0c7b6
RoneyThomas <roneythomas6@gmail.com>
2016-11-02 23:05:46 -0400

    Added bold style to the time shown in card

--- app/src/main/java/ca/stmarysorthodoxchurch/churchadmin/helper/ListBinder.java
+++ app/src/main/java/ca/stmarysorthodoxchurch/churchadmin/helper/ListBinder.java
@@ -2,11 +2,12 @@ package ca.stmarysorthodoxchurch.churchadmin.helper;
 
 import android.content.Context;
 import android.databinding.BindingAdapter;
+import android.graphics.Typeface;
+import android.text.SpannableStringBuilder;
+import android.text.style.StyleSpan;
 import android.view.LayoutInflater;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
-import java.util.ArrayList;
 
 import ca.stmarysorthodoxchurch.churchadmin.R;
 import ca.stmarysorthodoxchurch.churchadmin.models.Schedule;
@@ -24,8 +25,18 @@ public class ListBinder {
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         if (schedule != null) {
             for (int a = 0; a < schedule.getEvents().size(); a++) {
+                String x = schedule.getTimes().get(a) + ":  " + schedule.getEvents().get(a);
+                if (schedule.getTimes().get(a).length() < 8) {
+                    x = "  " + x;
+                }
+                SpannableStringBuilder str = new SpannableStringBuilder(x);
+                str.setSpan(new StyleSpan(Typeface.BOLD),
+                        0,
+                        10,
+                        SpannableStringBuilder.SPAN_EXCLUSIVE_EXCLUSIVE
+                );
                 TextView textView = (TextView) inflater.inflate(R.layout.event_list_item, null);
-                textView.setText(schedule.getTimes().get(a)+":    "+schedule.getEvents().get(a));
+                textView.setText(str);
                 layout.addView(textView);
             }
         }
